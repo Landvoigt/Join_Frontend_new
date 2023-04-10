31d2fe414456ba96e1e76b86a18b3ef43faa27fc
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-04-10 21:26:39 +0200

    change password visibility in login js added

--- js/login.js
+++ js/login.js
@@ -1,11 +1,34 @@
-function showPassword() {
-    let password = document.getElementById('passwordInput');
-    let passwordImg = document.getElementById('passwordImg');
+let inputPass = false;
+
+async function changePWSymbol() {
+  let InputField = document.getElementById("passwordInput");
+  let Symbol = document.getElementById("passwordImg");
+  if (InputField.value == "") {
+    Symbol.src = "../img/pasword.svg";
+    Symbol.classList.remove("pointer");
+    InputField.type = "password";
+    inputPass = false;
+  } else if ((InputField.type = "password")) {
+    Symbol.src = "../img/privacy.png";
+    Symbol.classList.add("pointer");
+    inputPass = true;
+  } else {
+    Symbol.src = "../img/visibility.svg";
+    Symbol.classList.add("pointer");
+    inputPass = true;
+  }
+}
+
+async function visibilityPass() {
+  let password = document.getElementById("passwordInput");
+  let passSymbol = document.getElementById("passwordImg");
+  if (inputPass === true) {
     if (password.type === "password") {
-        password.type = "text";
-        passwordImg.src = `./img/privacy.png`;
-      } else {
-        password.type = "password";
-        passwordImg.src = `./img/visibility.png`;
-      }
+      password.type = "text";
+      passSymbol.src = "../img/visibility.png";
+    } else {
+      password.type = "password";
+      passSymbol.src = "../img/privacy.png";
+    }
+  }
 }
