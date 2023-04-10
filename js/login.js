58961b4b0ebfd33756abf0e9730559b09043ead6
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-04-10 23:25:54 +0200

    added sign up card

--- js/login.js
+++ js/login.js
@@ -1,5 +1,12 @@
 let inputPass = false;
 
+function renderLogin() {
+  let card = document.getElementById('loginForm');
+  card.innerHTML = loginTemplate();
+  let header = document.getElementById('loginHeaderRight');
+  header.classList.remove("d-none");
+}
+
 async function changePWSymbol() {
   let InputField = document.getElementById("passwordInput");
   let Symbol = document.getElementById("passwordImg");
@@ -31,4 +38,69 @@ async function visibilityPass() {
       passSymbol.src = "../img/privacy.png";
     }
   }
+}
+
+function loginTemplate() {
+  return `
+            <div class="cardHeaderContainer">
+                <h1 class="loginH1">Log In</h1>
+                <span class="underlineForH1"></span>
+            </div>
+            <div class="loginInputFields">
+                <div class="loginInputField">
+                    <input class="loginE-Mail" type="text" required placeholder="E-mail">
+                    <img class="inputImg" src="../img/email.svg" alt="E-Mail">
+                </div>
+                <div class="loginInputField">
+                    <input class="loginE-Mail" type="password" required id="passwordInput" placeholder="Password" onkeydown="changePWSymbol()">
+                    <img class="inputImg passwordImg" id="passwordImg" src="../img/pasword.svg" alt="Password" onclick="visibilityPass()">
+                </div>
+                <div class="loginFooter">
+                    <input class="loginCheckbox margin-left-rifht-5 margin pointer" type="checkbox"> 
+                    <p class="rememberMe margin-left-rifht-5 margin">Remember me</p>
+                    <p class="forgotPass margin-left-rifht-5 margin pointer">Forgot my Password</p> 
+                </div>
+            </div>
+            <div class="loginFooterBtns">
+                <button class="loginBtn pointer">Log in</button>
+                <button class="guestBtn pointer">Guest Log in</button>
+            </div>
+  `;
+}
+
+function signUp() {
+  let header = document.getElementById('loginHeaderRight');
+  header.classList.add("d-none");
+  let card = document.getElementById('loginForm');
+  card.innerHTML = signUpTemplate();
+}
+
+function signUpTemplate() {
+  return `
+            <div class="signupHeaderContainer">
+              <div class="backImgContainer">  
+                <img onclick="renderLogin()" class="backImg pointer" src="../img/arrow-left.png" alt="Back">
+              </div> 
+                <h1 class="loginH1">Sign up</h1>
+                <span class="underlineForH1"></span>
+            </div>
+            <div class="loginInputFields">
+                <div class="loginInputField">
+                    <input class="loginE-Mail" type="text" required placeholder="Name">
+                    <img class="inputImg" src="../img/human-profile.png" alt="E-Mail">
+                </div>
+                <div class="loginInputField">
+                    <input class="loginE-Mail" type="text" required placeholder="Email">
+                    <img class="inputImg" src="../img/email.svg" alt="E-Mail">
+                </div>
+                <div class="loginInputField">
+                    <input class="loginE-Mail" type="password" required id="passwordInput" placeholder="Password" onkeydown="changePWSymbol()">
+                    <img class="inputImg passwordImg" id="passwordImg" src="../img/pasword.svg" alt="Password" onclick="visibilityPass()">
+                </div>
+                
+            </div>
+            <div class="signupFooterBtn">
+                <button class="loginBtn pointer">Sign up</button>
+            </div>
+  `;
 }
