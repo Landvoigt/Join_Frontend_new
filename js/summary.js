8b68493e6fd14ce169a3dae8fb48c6f654121e51
Kaser Erazer <kaser@live.de>
2023-04-13 22:33:04 +0200

    Added function to greet according to time

--- js/summary.js
+++ js/summary.js
@@ -1,10 +1,19 @@
 function loadDate() {
     const currentDate = new Date();
+    let dateContainer = document.getElementById('date');
+    const currenthour = currentDate.getHours();
     const formattedDate = currentDate.toLocaleDateString('en-US', {
         month: 'long',
         day: 'numeric',
         year: 'numeric'
     });
-    let dateContainer = document.getElementById('date');
     dateContainer.innerHTML = formattedDate;
+    console.log(currentDate.getHours())
+    if (currenthour < 12) {
+        document.getElementById('greeting').innerHTML='Good Morning';
+    }if (currenthour < 18) {
+        document.getElementById('greeting').innerHTML='Good Afternoon';
+    }else{
+        document.getElementById('greeting').innerHTML='Good Evening';
+    }
 }
