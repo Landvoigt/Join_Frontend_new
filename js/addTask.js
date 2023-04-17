9ebc9d8fa5df1c387a7f691a150f993fe0b5c221
Landvoigt <tim.voigt@ik.me>
2023-04-17 20:34:09 +0200

    finished colorpick for new categories

--- js/addTask.js
+++ js/addTask.js
@@ -21,6 +21,7 @@ let topics = [
     }
 ];
 let showCheckBoxes = true;
+let currentPickedColor;
 
 function addPrioColor(id) {
     let element = document.getElementById(id);
@@ -224,20 +225,78 @@ function removeAddTaskWindow() {
 }
 
 function createNewContactInAddTask() {
-    
+
 }
 
-function createNewCategoryInAddTask() { 
+function createNewCategoryInAddTask() {
     let dropdown = document.getElementById('categoryDropdownSection');
     dropdown.innerHTML = `
         <h4 class="addTask-form-headlines">Category</h4>
         <div class="dropdown grey-text">
-            New Category Name
+            <input id="new-cat-input" class="new-cat-input" maxlength="16" placeholder="New Category Name">
             <div class="create-cat-icon-box">
-                <img src="./img/plus.png" class="create-category-icon resize-icon">
+                <img src="./img/plus.png" class="create-category-icon resize-icon" onclick="resetAddCategorySection()">
                 <div class="gap-line"></div>
-                <img src="./img/check_mark.png" class="create-category-icon">
+                <img src="./img/check_mark.png" class="create-category-icon" onclick="addCategory()">
             </div>
         </div>
+        <div class="new-cat-color-select-box">
+            <div id="pickColor1" class="addTask-category-dot dot-hover pointer" style="background-color:red;" 
+            onclick="addBorderToPickedColor('pickColor1'); currentPickedColor = 'red'"></div>
+            <div id="pickColor2" class="addTask-category-dot dot-hover pointer" style="background-color:orange;" 
+            onclick="addBorderToPickedColor('pickColor2'); currentPickedColor = 'orange'"></div>
+            <div id="pickColor3" class="addTask-category-dot dot-hover pointer" style="background-color:lightgreen;" 
+            onclick="addBorderToPickedColor('pickColor3'); currentPickedColor = 'lightgreen'"></div>
+            <div id="pickColor4" class="addTask-category-dot dot-hover pointer" style="background-color:lightblue;" 
+            onclick="addBorderToPickedColor('pickColor4'); currentPickedColor = 'lightblue'"></div>
+            <div id="pickColor5" class="addTask-category-dot dot-hover pointer" style="background-color:yellow;" 
+            onclick="addBorderToPickedColor('pickColor5'); currentPickedColor = 'yellow'"></div>
+            <div id="pickColor6" class="addTask-category-dot dot-hover pointer" style="background-color:aqua;" 
+            onclick="addBorderToPickedColor('pickColor6'); currentPickedColor = 'aqua'"></div>
+            <div id="pickColor7" class="addTask-category-dot dot-hover pointer" style="background-color:grey;" 
+            onclick="addBorderToPickedColor('pickColor7'); currentPickedColor = 'grey'"></div>
+        </div>
+    `;
+}
+
+function resetAddCategorySection() {
+    let select = document.getElementById('categoryDropdownSection');
+    select.innerHTML = `
+            <h4 class="addTask-form-headlines">Category</h4>
+            <div id="categoryDropdown" class="dropdown" onclick="showSelection1()">
+                Select task category
+            </div>
+            <div class="category-selection" id="categorySelection">
+                <label class="addTask-category-label" onclick="createNewCategoryInAddTask()">
+                    <span>Create new category</span>
+                </label>
+            </div>
+    `;
+    generateTaskCategories();
+}
+
+function addBorderToPickedColor(id) {
+    const colors = document.querySelectorAll('.dot-hover');
+    for (let i = 0; i < colors.length; i++) {
+        colors[i].classList.remove('color-dot-bg');
+    }
+    let pickedColor = document.getElementById(`${id}`);
+    pickedColor.classList.add('color-dot-bg');
+}
+
+function addCategory() {
+    let newCat = document.getElementById('new-cat-input');
+    topics.push(
+        {
+            'name': `${newCat.value}`,
+            'color': `${currentPickedColor}`
+        }
+    )
+    resetAddCategorySection();
+    document.getElementById('categoryDropdown').innerHTML = `
+    <div style="display:flex; align-items:center;">
+        <span>${newCat.value}</span>
+        <div class="addTask-category-dot" style="background-color:${currentPickedColor};"></div>
+    </div>
     `;
 }
