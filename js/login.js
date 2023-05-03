8dbb638e602187df755d25aeeebc16af51b21f0d
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-05-03 20:50:54 +0200

    signUp responsive again

--- js/login.js
+++ js/login.js
@@ -91,7 +91,7 @@ function signUp() {
 
 function signUpTemplate() {
   return `
-          <form onsubmit="register(); return false;">
+          <form class="signUpForm" onsubmit="register(); return false;">
             <div class="signupHeaderContainer">
               <div class="backImgSignUp">  
                 <img onclick="renderLogin()" class="backImg pointer" src="../img/arrow-left.png" alt="Back">
